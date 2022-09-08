01df5160092bb61deac4ab08e9a6b12722b21402
yhunlu <yhunlu87@gmail.com>
2022-09-09 00:40:36 +0300

    added new schema as experiences.

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -7,6 +7,7 @@ import testimonials from './testimonials';
 import abouts from './abouts';
 import brands from './brands';
 import contact from './contact';
+import experiences from './experiences';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,5 +15,5 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([testimonials, abouts, brands, contact]),
+  types: schemaTypes.concat([testimonials, abouts, brands, contact, experiences]),
 });
